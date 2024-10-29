fc9223477c1ab56bd400c83212b16b7c09e0ba8f
FranM4rt <mjuanfrancisco45@gmail.com>
2024-10-29 12:01:07 -0300

    Corrigiendo error en Inventory

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Inventory extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'product_id',
+        'quantity',
+        'min_quantity',
+        'max_quantity',
+        'storage_location',
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
