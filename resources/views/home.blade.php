a88059160b3272d387b95b25a5223669343b057b
FranM4rt <mjuanfrancisco45@gmail.com>
2024-11-14 20:01:31 -0300

    agregando componente de carrusel para la página de inicio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,13 @@
+@php
+$images = [
+    ['src' => 'home-carousel/DC_20241018174428_XdEjhTqS.png', 'alt' => 'Image 1 description'],
+    ['src' => 'home-carousel/DC_20241018174550_a2KBtWsN.png', 'alt' => 'Image 2 description'],
+    ['src' => 'home-carousel/DC_20241022151949_kdqhRLmI.png', 'alt' => 'Image 3 description']
+];
+@endphp
+
 <x-home-layout>
+    <x-home.carousel :images="$images" />
 
     <div class="flex items-center justify-center h-96">
         <h1 class="text-4xl font-bold text-gray-700">Aca va el $slot</h1>
